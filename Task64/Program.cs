fad1f4c373091a5e8858f0abe8ace4ec6bac0c41
Gorelkina Katya <teterkinaejaterina@gmail.com>
2023-06-20 11:55:11 +0300

    done Task 64

---
+++ Task64/Program.cs
@@ -0,0 +1,11 @@
+﻿string printNumbers(int n)
+{
+    if (n == 1)
+        return $"{n}";
+    return $"{n}, " + printNumbers(n - 1);
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(printNumbers(n));
