66ac8bb3e45d6e085e07f9fe7653a50cbf61ce11
Gorelkina Katya <teterkinaejaterina@gmail.com>
2023-06-20 12:10:48 +0300

    done Task 66

---
+++ Task66/Program.cs
@@ -0,0 +1,13 @@
+﻿int sumNumbers(int n, int m)
+{
+    if (n == m)
+        return n;
+    return sumNumbers(n + 1, m - 1) + n + m;
+}
+
+Console.Clear();
+Console.Write("Введите 1-e число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-e число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine(sumNumbers(n, m));
